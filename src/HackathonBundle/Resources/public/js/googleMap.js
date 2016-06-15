d89c680b1f21c038346aaf1dce3d50a1c9a55bfe
Emilie Chartier <emilie.chartier@sfr.fr>
2016-06-15 23:30:30 +0200

    Modifs API

--- src/HackathonBundle/Resources/public/js/googleMap.js
+++ src/HackathonBundle/Resources/public/js/googleMap.js
@@ -1,4 +1,10 @@
-
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
+}
 
 function initialize() {
     var pyrmont = new google.maps.LatLng(-33.8665, 151.1956);
@@ -37,4 +43,36 @@ function initialize() {
 
 // Run the initialize function when the window has finished loading.
 google.maps.event.addDomListener(window, 'load', initialize);
+
+
+var map;
+var service;
+var infowindow;
+
+function initialize() {
+    var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: pyrmont,
+        zoom: 15
+    });
+
+    var request = {
+        location: pyrmont,
+        radius: '500',
+        types: ['store']
+    };
+
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
+}
+
+function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            var place = results[i];
+            createMarker(results[i]);
+        }
+    }
+}
 
